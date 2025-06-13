119020697c4281c0846b824e6314c8f020c0b739
angel-palkina <s.palkina@innopolis.university>
2025-06-13 16:20:04 +0500

    feat: add functions for creating, searching and deleting users

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 import os
-from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine
+from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine, TIMESTAMP
 from databases import Database
 from dotenv import load_dotenv
+from datetime import datetime
 
 # load environment variables
 load_dotenv(os.path.join(os.path.dirname(__file__), '../database/.env'))
@@ -17,6 +18,16 @@ games = Table(
     Column("assigned_id", String(50), unique=True),
     Column("password", String(100)),
     Column("game_state", String(20)),
+)
+
+people = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("login", String(50), unique=True),
+    Column("password", String(100)),
+    Column("created_at", TIMESTAMP(timezone=True), default=datetime.utcnow),
+    Column("last_visited", TIMESTAMP(timezone=True), default=datetime.utcnow),
 )
 
 database = Database(DATABASE_URL)
@@ -35,4 +46,25 @@ async def delete_game_room(room_id: int):
 # for searching room
 async def search_game_room(assigned_id: str):
     query = games.select().where(games.c.assigned_id == assigned_id)
+    return await database.fetch_one(query)
+
+
+
+# for creating user
+async def create_user(id: int, login: str, password: str, created_at: datetime | None = None, last_visited: datetime | None = None ):
+    query = users.insert().values(id=id, login = login, password=password, created_at=created_at or datetime.utcnow(), last_visited = last_visited or datetime.utcnow())
+    return await database.execute(query)
+
+# for deleting user
+async def delete_user(user_id: int):
+    query = users.delete().where(users.c.id == user_id)
+    return await database.execute(query)
+
+# for searching user
+async def search_user_by_id(user_id: int):
+    query = users.select().where(users.c.id == user_id)
+    return await database.fetch_one(query)
+
+async def search_user_by_login(login: str):
+    query = users.select().where(users.c.login == login)
     return await database.fetch_one(query)
