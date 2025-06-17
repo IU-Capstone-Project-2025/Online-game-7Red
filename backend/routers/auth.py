b0ed0446af356c32adc1a79c57337c3738b04841
Polina <kostikova658@gmail.com>
2025-06-17 16:07:11 +0300

    feat: implemented registration and authorization

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,16 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from backend.models import SignInRequest, SignUpRequest
+from backend.database import create_user, search_user_by_login, create_profile
+from passlib.context import CryptContext
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 @router.post("/signup")
 async def signup(request: SignUpRequest):
-    #
-    return {"message": "Not implemented yet"}
+    # Check if the user alredy registered
+    exist = await search_user_by_login(request.email)
+    if exist:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    if request.password != request.repeated_password:
+        raise HTTPException(status_code=400, detail="Passwords do not match")
+    # create safe hash of password before saving to database
+    hashed_password = pwd_context.hash(request.password)
+    user_id = await create_user(request.email, hashed_password)
+    await create_profile(user_id, request.nickname)
+    return {"message": "User reqistered succesfully"}
 
 
 @router.post("/signin")
 async def signin(request: SignInRequest):
-    #
-    return {"message": "Not implemented yet"}
+    user = await search_user_by_login(request.email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    if not pwd_context.verify(request.password, user["password"]):
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+    return {"message": "Sign in succesfully", "user_id":user["id"]}
 
