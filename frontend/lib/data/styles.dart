2deb8c80fb401cbf42637df9ecea05ef367c9eaa
Amir Bairamov <a.bairamov@innopolis.university>
2025-06-24 18:57:37 +0300

    feat: add moving and functions to GamePage, connection with backend

--- frontend/lib/data/styles.dart
+++ frontend/lib/data/styles.dart
@@ -48,6 +48,13 @@ TextStyle bigNumberStyle = const TextStyle(
   color: Color.fromARGB(255, 58, 58, 58)
 );
 
+TextStyle cardNumStyle = const TextStyle(
+  fontFamily: 'Awesome5',
+  fontSize: 48,
+  fontWeight: FontWeight.w500,
+  color: Colors.white
+);
+
 Color backInvisColor = const Color.fromARGB(125, 218, 209, 193);
 
 Color grey3A3A3AColor = const Color.fromARGB(255, 58, 58, 58);
@@ -62,4 +69,18 @@ Color invisColor = const Color.fromARGB(0, 255, 255, 255);
 
 Color redCard = const Color.fromARGB(255, 249, 61, 61);
 
+Color orangeCard = const Color.fromARGB(255, 255, 143, 31);
+
+Color yellowCard = const Color.fromARGB(255, 255, 232, 58);
+
+Color greenCard = const Color.fromARGB(255, 79, 224, 84);
+
+Color blueCard = const Color.fromARGB(255, 51, 218, 255);
+
+Color indigoCard = const Color.fromARGB(255, 77, 92, 255);
+
+Color violetCard = const Color.fromARGB(255, 202, 75, 253);
+
 Color handInvisColor = const Color.fromARGB(58, 212, 197, 172);
+
+Color greyButtonColor = const Color.fromARGB(255, 186, 180, 170);
--- frontend/lib/pages/game_page.dart
+++ frontend/lib/pages/game_page.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../data/styles.dart';
 import '../providers/provider.dart';
+import '../data/cards.dart';
+import '../socket/web_socket.dart';
+import '../data/player.dart';
 
 class GameRoomPage extends StatefulWidget {
   const GameRoomPage({super.key});
@@ -13,6 +18,265 @@ class GameRoomPage extends StatefulWidget {
 }
 
 class _GameRoomPageState extends State<GameRoomPage> {
+  String? roomID;
+  int? userID;
+  String serverUrl = '?';
+
+  int gamemode = 2;
+
+  late GameWebSocket _webSocket;
+  List<String> _myHand = [];
+  List<Player> _players = [];
+  List<String> _pallete = [];
+  List<int> _activePlayers = [];
+  String _ruleCard = 'R0';
+  int _currentPlayerId = -1;
+  int _nextLose = 0;
+  Timer? _turnTimer;
+  int _timeLeft = 60;
+  String my_pallete_ch = '';
+
+  Player? playerUp;
+  Player? playerLeft;
+  Player? playerRight;
+
+  bool myTurn = false;
+  bool myAllTurn = false;
+  bool ruleChanged = false;
+  bool palleteChanged = false;
+
+  // List<String> myPallete = ["R7", "O6", "Y5", "G4", "B3"];
+  // List<String> myHand = ["G4", "B3", "I2", "V1"];
+  // String currRuleCard = "R0";
+  
+
+  @override
+  void initState() {
+    super.initState();
+    _connectToWebSocket();
+  }
+
+  void _connectToWebSocket() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    roomID = await prefs.getString('roomId');
+    userID = await prefs.getInt('myID');
+    serverUrl = 'http://localhost:8000/game/$roomID/ws';
+    _webSocket = GameWebSocket(
+      serverUrl: serverUrl,
+      onMessageReceived: _handleMessage,
+      onConnectionClosed: _onDisconnected,
+    );
+    _webSocket.connect();
+  }
+
+  void _handleMessage(dynamic message) {  //мб тут не так будет соо отправляться (не то передаю)
+    switch (message['type']) {
+      case 'initialized':
+        _handleInitialized(message);
+        break;
+      case 'wrong_turn':
+        _handleWrongTurn(message);
+        break;
+      case 'right_turn':
+        _handleRightTurn();
+        break;
+      case 'change_turn':
+        _handleChangeTurn(message);
+        break;
+    }
+  }
+
+  void _handleInitialized(Map<String, dynamic> message) {
+    setState(() {
+      _myHand = List<String>.from(message['my_hand']);
+      _players = List.generate(
+        message['names'].length,
+        (index) => Player(
+          id: message['id'][index],
+          name: message['names'][index],
+          isMe: message['id'][index] == userID,
+        ),
+      );
+      _activePlayers = message['id'];
+      _currentPlayerId = message['id'][0];
+
+
+      final player = _players.firstWhere((p) => p.id == _activePlayers[0]);
+      
+      if (player.isMe) {
+        myTurn = true;
+        palleteChanged = false;
+        ruleChanged = false;
+        my_pallete_ch = "";
+        myAllTurn = true;
+      }
+      
+      if (gamemode == 2) {
+        if (player.isMe == false ) {
+          playerUp = player;
+        } else {
+          playerUp = _players.firstWhere((p) => p.id == _activePlayers[1]);
+        }
+      }
+      
+      _startTurnTimer();
+    });
+  }
+
+  void _handleWrongTurn(Map<String, dynamic> message) {
+    // Вернуть карты, если ход неверный
+    setState(() {
+      if (message['my_pallete_ch'] != null) {
+        _pallete.remove(message['my_pallete_ch']);
+        _myHand.add(message['my_pallete_ch']);
+      }
+      if (message['rule_ch'] != null) {
+        _myHand.add(message['rule_ch']);
+      }
+      _ruleCard = message['old_rule'];
+      myTurn = true;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Недопустимый ход!')),
+    );
+  }
+
+  void _handleRightTurn() {
+    // Ход принят, ждем смены хода
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Ход принят!')),
+    );
+    setState(() {
+      myTurn = false;
+      ruleChanged = true;
+      palleteChanged = true;
+      myAllTurn = false;
+    });
+  }
+
+  void _handleChangeTurn(Map<String, dynamic> message) {
+    setState(() {
+      final player = _players.firstWhere((p) => p.id == message['id_did']);
+
+      if (message['lose'] == 1) {
+        if (gamemode == 2) {
+          if (player.id == playerUp!.id) {
+            playerUp!.pallete = [];
+          }
+        }
+        _activePlayers.remove(message['id_did']);
+        if (_activePlayers.length == 1) {
+          // TODO: Закончить игру с победой
+        }
+      } else {
+        if (message['his_pallete_ch'] != null) {
+          if (gamemode == 2) {
+            if (player.id == playerUp!.id) {
+              playerUp!.pallete.add(message['his_pallete_ch']);
+            }
+          }
+        }
+      }
+      if (message['rule_ch'] != null) {
+        _ruleCard = message['rule_ch'];
+      }
+      _nextLose = message['next_lose'];
+      _currentPlayerId = nextPlayerId(_currentPlayerId);
+      if (_players.firstWhere((p) => p.id == _currentPlayerId).isMe) {
+        if (_nextLose == 1) {
+          //TODO: Закончить игру c поражением
+          _pallete = [];
+          _myHand = [];
+        }
+        myTurn = true;
+        palleteChanged = false;
+        ruleChanged = false;
+        my_pallete_ch = "";
+        myAllTurn = true;
+      }
+      
+      _startTurnTimer();
+    });
+  }
+
+  void _startTurnTimer() {
+    _turnTimer?.cancel();
+    setState(() => _timeLeft = 60);
+    _turnTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (_timeLeft > 0) {
+        setState(() => _timeLeft--);
+      } else {
+        timer.cancel();
+        // Автоматически завершаем ход, если время вышло
+        if (myTurn) {
+          _submitTurnTimeout();
+        }
+      }
+    });
+  }
+
+  void _submitTurn() {
+    _turnTimer?.cancel();
+
+    final message = {
+      'type': 'my_turn',
+      'my_id': userID,
+      'my_room': roomID,
+      'my_pallete_ch': palleteChanged ? my_pallete_ch : null,
+      'rule_ch': ruleChanged ? _ruleCard : null,
+      'my_hand': _myHand,
+      'pallete': _pallete,
+    };
+
+    _webSocket.sendMessage(message);
+
+    setState(() {
+      myTurn = false;
+    });
+  }
+
+  void _submitTurnTimeout() {
+    _turnTimer?.cancel();
+
+    final message = {
+      'type': 'time_out',
+      'my_id': userID,
+      'my_room': null,
+      'my_pallete_ch': null,
+      'rule_ch': null,
+      'my_hand': null,
+      'pallete': null,
+    };
+
+    _webSocket.sendMessage(message);
+
+    setState(() {
+      _myHand = [];
+      _pallete = [];
+      myTurn = false;
+      //TODO: выйти из игры
+    });
+  }
+
+  int nextPlayerId(int currID) {
+    final index = _activePlayers.indexOf(currID);
+    return _activePlayers[(index + 1) % _activePlayers.length];
+  }
+
+  void _onDisconnected() {
+    // Обработка разрыва соединения
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Соединение с сервером потеряно')),
+    );
+  }
+
+  @override
+  void dispose() {
+    _turnTimer?.cancel();
+    _webSocket.disconnect();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     final gameProvider = Provider.of<GameProvider>(context);
@@ -63,7 +327,7 @@ class _GameRoomPageState extends State<GameRoomPage> {
                         children: [
                           Icon(Icons.filter_7, size: 24, color: grey3A3A3AColor,),
                           Padding(padding: const EdgeInsets.only(right: 5),),
-                          Text("Player_ABC", style: buttonTextStyle),
+                          Text(playerUp!.name, style: buttonTextStyle),
                         ],
                       )
                     ]
@@ -77,6 +341,7 @@ class _GameRoomPageState extends State<GameRoomPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
 //--------------------LEFT-PLAYER----------------------------------------------------------------------------------------------------------
+                  if (gamemode == 3 || gamemode == 4)
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -90,7 +355,9 @@ class _GameRoomPageState extends State<GameRoomPage> {
                         ],
                       )
                   ]),
+                  if (gamemode == 3 || gamemode == 4)
                   Padding(padding: const EdgeInsets.only(right: 30)),
+                  if (gamemode == 3 || gamemode == 4)
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -121,7 +388,14 @@ class _GameRoomPageState extends State<GameRoomPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          for (int i = 0; i < 7; i++)
+                          for (int i = 0; i < playerUp!.pallete.length; i++)
+                                Row(
+                                  children: [
+                                    CentralCardWidget(card: playerUp!.pallete[i]),
+                                    Padding(padding: const EdgeInsets.only(left: 18)),
+                                  ],
+                                ),
+                          for (int i = playerUp!.pallete.length; i < 7; i++)
                             Row(
                               children: [
                                 Container(
@@ -148,42 +422,71 @@ class _GameRoomPageState extends State<GameRoomPage> {
                             height: 179,
                           ),
                           Padding(padding: const EdgeInsets.only(left: 32)),
-                          Container(
-                                  height: 84,
-                                  width: 60,
-                                  decoration:  BoxDecoration(
-                                    color: redCard,
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(color: grey3A3A3AColor, width: 1.5),
-                                  ),
-                                ),
+                          DragTarget<String>(
+                            onWillAcceptWithDetails: (data) {
+                              return !ruleChanged;
+                            },
+                            onAcceptWithDetails: (data) {
+                              setState(() {
+                                _ruleCard = data.data;
+                                ruleChanged = true;
+                                _myHand.remove(data.data);
+                              });
+                            },
+                            builder: (context, candidateItems, rejectedItems) {
+                              return CentralCardWidget(card: _ruleCard);
+                            }
+                          ),
                         ],
                       ),
                       Padding(padding: const EdgeInsets.only(top: 75)),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          for (int i = 0; i < 7; i++)
-                            Row(
-                              children: [
-                                Container(
-                                  height: 84,
-                                  width: 60,
-                                  decoration:  BoxDecoration(
-                                    color: invisColor,
-                                    borderRadius: BorderRadius.circular(5),
-                                    border: Border.all(color: grey3A3A3AColor, width: 1.5),
-                                  ),
+                      DragTarget<String>(
+                        onWillAcceptWithDetails: (data) {
+                          return !palleteChanged;
+                        },
+                        onAcceptWithDetails: (data) {
+                          setState(() {
+                            _pallete.add(data.data);
+                            palleteChanged = true;
+                            my_pallete_ch = data.data;
+                            _myHand.remove(data.data);
+                          });
+                        },
+                        builder: (context, candidateData, rejectedData) {
+                          return Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              for (int i = 0; i < _pallete.length; i++)
+                                Row(
+                                  children: [
+                                    CentralCardWidget(card: _pallete[i]),
+                                    Padding(padding: const EdgeInsets.only(left: 18)),
+                                  ],
                                 ),
-                                Padding(padding: const EdgeInsets.only(left: 18)),
-                              ],
-                            )
-                        ],
-                      ),
+                              for (int i = _pallete.length; i < 7; i++)
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 84,
+                                      width: 60,
+                                      decoration:  BoxDecoration(
+                                        color: invisColor,
+                                        borderRadius: BorderRadius.circular(5),
+                                        border: Border.all(color: grey3A3A3AColor, width: 1.5),
+                                      ),
+                                    ),
+                                    Padding(padding: const EdgeInsets.only(left: 18)),
+                                  ],
+                                )
+                            ],
+                          );
+                        },
+                      )
                     ],
                   ),
 //--------------------RIGHT-PLAYERS----------------------------------------------------------------------------------------------------------
                   Padding(padding: const EdgeInsets.only(right: 44)),
+                  if (gamemode == 3 || gamemode == 4)
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -206,7 +509,9 @@ class _GameRoomPageState extends State<GameRoomPage> {
                       Padding(padding: const EdgeInsets.only(bottom: 21)),
                     ],
                   ),
+                  if (gamemode == 3 || gamemode == 4)
                   Padding(padding: const EdgeInsets.only(right: 30)),
+                  if (gamemode == 3 || gamemode == 4)
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -238,7 +543,27 @@ class _GameRoomPageState extends State<GameRoomPage> {
                       child: Icon(Icons.account_circle, size: 90, color: grey3A3A3AColor,),
                     ),
                     Padding(padding: const EdgeInsets.only(left: 29)),
-                    for (int i = 0; i < 7; i++)
+                    for (int i = 0; i < _myHand.length; i++)
+                      Row(
+                        children: [
+                          Draggable<String>(
+                            data: _myHand[i],
+                            feedback: CentralCardWidget(card: _myHand[i]),
+                            childWhenDragging: Container(
+                              height: 106,
+                              width: 71,
+                              decoration:  BoxDecoration(
+                                color: invisColor,
+                                borderRadius: BorderRadius.circular(5),
+                                border: Border.all(color: grey3A3A3AColor, width: 1.5),
+                              ),
+                            ),
+                            child: HandCardWidget(card: _myHand[i]),
+                          ),
+                          Padding(padding: const EdgeInsets.only(left: 18)),
+                        ],
+                      ),
+                    for (int i = _myHand.length; i < 7; i++)
                       Row(
                         children: [
                           Container(
@@ -259,14 +584,14 @@ class _GameRoomPageState extends State<GameRoomPage> {
                       height: 50,
                       child: ElevatedButton(
                         style: ButtonStyle(
-                          backgroundColor: WidgetStateProperty.all<Color>(buttonColor),
+                          backgroundColor: WidgetStateProperty.all<Color>(myAllTurn ? (myTurn ? buttonColor : greyButtonColor) : invisColor,),
                           textStyle: WidgetStateProperty.all<TextStyle>(buttonTextStyle,),
-                          foregroundColor: WidgetStateProperty.all<Color>(grey3A3A3AColor,),
+                          foregroundColor: WidgetStateProperty.all<Color>(myAllTurn ? (myTurn ? grey3A3A3AColor : Colors.white) : invisColor,),
                           shape: WidgetStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30),),),
-                          side: WidgetStateProperty.all<BorderSide>(BorderSide(color: grey3A3A3AColor, width: 1),),
+                          side: WidgetStateProperty.all<BorderSide>(BorderSide(color: myAllTurn ?  grey3A3A3AColor : invisColor, width: 1),),
                         ),
-                        onPressed: () async{
-                          // send request to server
+                        onPressed: () {
+                          _submitTurn();
                         },
                       child: Text('SUBMIT'),
                       ),
