5c640d211072dd3ed7ff34b06098a9dc14eb27bd
Amir Bairamov <a.bairamov@innopolis.university>
2025-06-29 19:55:58 +0300

    fix: fix error alerts when user tru to commect with wrong ip of passord

--- frontend/lib/pages/main_menu_page.dart
+++ frontend/lib/pages/main_menu_page.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 
 import '../data/styles.dart';
 import '../providers/provider.dart';
+import '../data/connectDialog.dart';
 
 class MainMenuPage extends StatefulWidget {
   const MainMenuPage({super.key});
@@ -254,140 +255,10 @@ class _MainMenuPageState extends State<MainMenuPage> {
                                       ),
                                       onPressed: () {
                                         Navigator.of(context).pop();
-                                        showDialog(context: context, builder: (context) {
-                                          return Dialog(
-                                            child:
-                                              Container(
-                                                width: 420,
-                                                height: 308,
-                                                decoration: BoxDecoration(
-                                                  image: DecorationImage(
-                                                    image: AssetImage('lib/assets/background.jpg'),
-                                                    fit: BoxFit.cover,
-                                                  ),
-                                                  borderRadius: BorderRadius.circular(20),
-                                                  border: Border.all(color: grey3A3A3AColor, width: 0.1),
-                                                ),
-                                                child:
-                                                  Container(
-                                                    width: 420,
-                                                    height: 308,
-                                                    decoration: BoxDecoration(
-                                                      color: backInvisColor,
-                                                      borderRadius: BorderRadius.circular(20),
-                                                      border: Border.all(color: grey3A3A3AColor, width: 0.1),
-                                                    ),
-                                                    child: Column(
-                                                      children: [
-                                                        Padding(padding: const EdgeInsets.only(top: 30)),
-                                                        Row(
-                                                          mainAxisAlignment: MainAxisAlignment.center,
-                                                          children: [
-                                                            Padding(padding: const EdgeInsets.only(left: 47)),
-                                                            Text("ID of the game-room", style: basicTextStyle,),
-                                                            const Expanded(flex: 1, child: Text("")),
-                                                          ]
-                                                        ),
-                                                        Padding(padding: const EdgeInsets.only(top: 5)),
-                                                        Container(
-                                                          width: 327,
-                                                          height: 40,
-                                                          decoration: BoxDecoration(
-                                                            color: Colors.white,
-                                                            borderRadius: BorderRadius.circular(8),
-                                                          ),
-                                                          child: TextField(
-                                                            decoration: const InputDecoration(
-                                                              border: OutlineInputBorder(
-                                                                borderSide: BorderSide.none,
-                                                              ),
-                                                            ),
-                                                            controller: controller,
-                                                          ),
-                                                        ),
-                                                        Padding(padding: const EdgeInsets.only(top: 30)),
-                                                        Row(
-                                                          mainAxisAlignment: MainAxisAlignment.center,
-                                                          children: [
-                                                            Padding(padding: const EdgeInsets.only(left: 47)),
-                                                            Text("Password", style: basicTextStyle),
-                                                            const Expanded(flex: 1, child: Text("")),
-                                                          ]
-                                                        ),
-                                                        Padding(padding: const EdgeInsets.only(top: 5)),
-                                                        Container(
-                                                          width: 327,
-                                                          height: 40,
-                                                          decoration: BoxDecoration(
-                                                            color: Colors.white,
-                                                            borderRadius: BorderRadius.circular(8),
-                                                          ),
-                                                          child: TextField(
-                                                            decoration: const InputDecoration(
-                                                              border: OutlineInputBorder(
-                                                                borderSide: BorderSide.none,
-                                                              ),
-                                                            ),
-                                                            controller: controller2,
-                                                          ),
-                                                        ),
-                                                        Padding(padding: const EdgeInsets.only(top: 30)),
-                                                        SizedBox(
-                                                            width: 327,
-                                                            height: 40,
-                                                            child: ElevatedButton(
-                                                              style: ButtonStyle(
-                                                                backgroundColor: WidgetStateProperty.all<Color>(
-                                                                  buttonColor,
-                                                                ),
-                                                                textStyle: WidgetStateProperty.all<TextStyle>(
-                                                                  buttonTextStyle,
-                                                                ),
-                                                                foregroundColor: WidgetStateProperty.all<Color>(
-                                                                  grey3A3A3AColor,
-                                                                ),
-                                                                shape:
-                                                                    WidgetStateProperty.all<RoundedRectangleBorder>(
-                                                                      RoundedRectangleBorder(
-                                                                        borderRadius: BorderRadius.circular(10),
-                                                                      ),
-                                                                    ),
-                                                                side: WidgetStateProperty.all<BorderSide>(
-                                                                  BorderSide(color: grey3A3A3AColor, width: 1),
-                                                                ),
-                                                              ),
-                                                              onPressed: () async{
-                                                                setState(() {
-                                                                  postText = '';
-                                                                  logSuccess = false;
-                                                                });
-                                                                if (controller.text.isEmpty || controller2.text.isEmpty) {
-                                                                  setState(() {
-                                                                    postText = "All fields are required";
-                                                                  }); // ???? WTF Почему не всплывает?..
-                                                                  return;
-                                                                } else {
-                                                                  await connectToRoom(gameProvider.myID, controller.text, controller2.text);
-                                                                  if (logSuccess) {
-                                                                    gameProvider.roomId = controller.text;
-                                                                    gameProvider.roomPassword = controller2.text;
-                                                                    gameProvider.saveRoomInfo();
-                                                                    Navigator.pushNamed(context, '/waitingroom');
-                                                                  }
-                                                                }
-                                                              },
-                                                              child: const Text('CONNECT'),
-                                                            ),
-                                                          ),
-                                                          Padding(padding: const EdgeInsets.only(top: 10)),
-                                                          Text(postText, style: errorTextStyle,),
-
-                                                      ],
-                                                    ),
-                                                  ),
-                                              )
-                                          );
-                                        });
+                                        showDialog(
+                                          context: context,
+                                          builder: (context) => ConnectDialog(gameProvider: gameProvider),
+                                        );
                                       },
                                       child: Column(
                                         children: [
