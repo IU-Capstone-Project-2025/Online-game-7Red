bdbdde90a612495d75503f3f15c9815134367ee9
Arina Petuhova <119685834+arinapetukhova@users.noreply.github.com>
2025-07-01 11:04:13 +0300

    fix: main.py to old version

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from backend.routers.auth import router as auth_router
 from backend.routers.rooms import router as rooms_router
 from backend.routers.game import router as game_router
@@ -12,28 +11,25 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],  # Для разработки можно "*", для прода лучше указать конкретные домены
     allow_credentials=True,
-    allow_methods=["*"],  
+    allow_methods=["*"],  # Или конкретные методы: ["GET", "POST", "OPTIONS", etc.]
     allow_headers=["*"],
 )
 
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+@app.on_event("startup")
+async def startup():
     await database.connect()
-    yield
-    await database.disconnect()
 
-app = FastAPI(lifespan=lifespan)
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 app.include_router(auth_router)
 app.include_router(rooms_router)
 app.include_router(game_router)
 #app.include_router(bot_router)
 #app.include_router(ai_game_router)
-
 
 @app.get("/")
 async def read_root():
