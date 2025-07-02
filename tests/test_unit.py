48961f374ce15f4dd2a788b0095e01402415833e
angel-palkina <s.palkina@innopolis.university>
2025-07-02 09:49:13 +0300

    fix relative path

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,5 +1,5 @@
 import pytest
-from backend.red7state import Red7GameState, CardColor, CARD_VALUES
+from ..backend.red7state import Red7GameState, CardColor, CARD_VALUES
 
 def test_initialize_deck_unique_cards():
     game = Red7GameState("test_room")
