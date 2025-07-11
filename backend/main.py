9cc4b3710dcb1953a012f6a3a9f7f59191d9c9a5
Arina Petuhova <119685834+arinapetukhova@users.noreply.github.com>
2025-07-11 13:41:35 +0300

    fix: fix exited player active/inactive state

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,6 @@ from backend.routers.game import router as game_router
 from backend.routers.game_bot import router as ai_game_router
 from backend.routers.settings import router as setting_router
 from backend.routers.avatar import router as avatar_router
-#from backend.routers.bot import router as bot_router
 
 #from backend.routers.game_bot import router as ai_game_router 
 from backend.database import database
