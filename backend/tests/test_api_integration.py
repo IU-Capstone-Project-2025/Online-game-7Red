eb609ecb64b00aeae16840985c2a16f4165f17f5
Polina <kostikova658@gmail.com>
2025-07-13 12:14:38 +0300

    fix: fix tests (delete info in db), rooms in online search are created for >=2 users, add delete endpoint

--- backend/tests/test_api_integration.py
+++ backend/tests/test_api_integration.py
@@ -26,6 +26,12 @@ def test_signup_and_create_room():
         assert "password" in data
         assert len(data["assigned_id"]) == 5
         assert len(data["password"]) == 5
+        leave = {"user_id": user_id, "assigned_id": data["assigned_id"]}
+        response = client.post("/api/rooms/leave", json=leave)
+        assert response.status_code == 200
+        response = client.post("/api/auth/delete", json={"user_id": user_id})
+        assert response.status_code == 200
+        
         
 def test_signup():
     with TestClient(app) as client:
@@ -41,6 +47,7 @@ def test_signup():
         data = response.json()
         assert data["message"] == "User reqistered succesfully"
         assert "user_id" in data
+        user_id = data["user_id"]
 
         # again register the same user
         response = client.post("/api/auth/signup", json=signup_data)
@@ -53,6 +60,8 @@ def test_signup():
         response = client.post("/api/auth/signup", json=signup_data)
         assert response.status_code == 400
         assert response.json()["detail"] == "Passwords do not match"
+        response = client.post("/api/auth/delete", json={"user_id": user_id})
+        assert response.status_code == 200
         
 def test_player_is_ready():
     with TestClient(app) as client:
@@ -78,6 +87,11 @@ def test_player_is_ready():
         response = client.post("/api/rooms/ready", json=ready)
         assert response.status_code == 200
         assert f"Player {user_id} is ready in room {assigned_id}" in response.json()["message"]
+        leave = {"user_id": user_id, "assigned_id": assigned_id}
+        response = client.post("/api/rooms/leave", json=leave)
+        assert response.status_code == 200
+        response = client.post("/api/auth/delete", json={"user_id": user_id})
+        assert response.status_code == 200
     
 def test_leave_room():
     with TestClient(app) as client:
@@ -107,6 +121,8 @@ def test_leave_room():
         response = client.post("/api/rooms/leave", json=leave)
         assert response.status_code == 200
         assert f"User {user_id} left room {assigned_id}" in response.json()["message"]
+        response = client.post("/api/auth/delete", json={"user_id": user_id})
+        assert response.status_code == 200
 
 def test_signin():
     with TestClient(app) as client:
@@ -145,3 +161,5 @@ def test_signin():
         })
         assert response.status_code == 401
         assert response.json()["detail"] == "Invalid email or password"
+        response = client.post("/api/auth/delete", json={"user_id": user_id})
+        assert response.status_code == 200
